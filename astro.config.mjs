b83882a431ecac72638f735d087316772839387c
Tamal Chowdhury <tamalanwaronline@gmail.com>
2024-06-06 18:29:59 +0600

    adds vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 /** @format */
 
 import { defineConfig } from "astro/config";
-
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [compress(), sitemap(), react()],
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
